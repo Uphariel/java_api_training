0fd381780f3632c0134d78944e02ddb3051fee38
Clément DURIN <clement.durin@epita.fr>
2021-07-11 10:11:08 +0200

    Ping fonctionnel

---
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -0,0 +1,17 @@
+package fr.lernejo.navy_battle;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class Launcher {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        if (args.length != 0) {
+            int serverPort = Integer.parseInt(args[0]);
+            Server server = new Server(UUID.randomUUID().toString(),"localhost:" + serverPort, "Coucou" );
+            server.startServer(serverPort);
+            int poteto = 0;
+        }
+
+    }
+}
---
+++ src/main/java/fr/lernejo/navy_battle/Server.java
@@ -0,0 +1,45 @@
+package fr.lernejo.navy_battle;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.Executors;
+
+public class Server {
+    public final String id;
+    public final String url;
+    public final String msg;
+
+    public Server(String id, String url, String msg) {
+        this.id = id;
+        this.url = url;
+        this.msg = msg;
+    }
+
+    public void startServer(int port) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.setExecutor(Executors.newSingleThreadExecutor());
+        server.createContext("/ping", this::pingHandler);
+        server.start();
+    }
+
+    private void pingHandler(HttpExchange exchange) throws IOException {
+        String body = "OK";
+        exchange.sendResponseHeaders(200, body.length());
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(body.getBytes());
+        }
+    }
+}
